a6b908455d6e65c92be02c5382cd88caf314c58e
jutianlei <wxq366@qq.com>
2024-01-23 11:11:19 +0800

    chore: 添加carco 配置

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const config = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // 自定义 webpack 配置
+      // 您可以在这里修改 webpackConfig 对象以满足您的需求
+      if (env === "production") {
+        // 设置 publicPath
+        webpackConfig.output.publicPath = "./jutainlei";
+      }
+
+      // 返回修改后的 webpack 配置
+      return webpackConfig;
+    },
+  },
+};
+
+module.exports = config;
